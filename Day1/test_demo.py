7e38c140555989cf510617b7a39aedc69b5966b5
evog-jpg <evog@cesar.org.br>
2025-09-19 08:47:45 -0300

    meu primeiro commit

--- Day1/test_demo.py
+++ Day1/test_demo.py
@@ -1,7 +1,3 @@
-from selenium import webdriver
-
 def test_verify_demoqa_title(driver):
-    driver = webdriver.Chrome()
     driver.get("https://demoqa.com")
-    #assert "DEMOQA" in driver.title
-    #driver.quit()
+    assert "DEMOQA" in driver.title
