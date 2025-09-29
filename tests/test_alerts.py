8e579e4aa1004a4618a21933773b8f738e7f66d0
evog-jpg <evog@cesar.org.br>
2025-09-29 12:00:41 -0300

    tirei os espeços em branco da primeira def

--- tests/test_alerts.py
+++ tests/test_alerts.py
@@ -4,11 +4,8 @@ from selenium.webdriver.common.by import By
 def test_alerts_interaction(driver):
     alerts_page = AlertsPage(driver)
     alerts_page.navigate()
-
     alerts_page.click_alerts_windows_button()
-
     alerts_page.goto_alerts_page()
-
     assert alerts_page.click_see_alert_button() == "You clicked a button"
 
 def test_alerts_5seconds(driver):
